7262e9c6f4b7471bc9e30c201e618cacdf5f50a5
Naimur300 <aymannaimur5@gmail.com>
2023-08-16 23:57:22 +0600

    responsive and validation added

--- js/deposit.js
+++ js/deposit.js
@@ -2,27 +2,37 @@ document.getElementById('btn-deposit').addEventListener('click' , function(){
     
     const depositField = document.getElementById('deposit-field');
     const newDepositedString = depositField.value 
-   
+    const newDeposited = parseFloat(newDepositedString)
+    if(isNaN(newDeposited) ){
+        alert("Please enter numbers")
+        depositField.value = ''
+    }
 
-    const initialDeposit =  document.getElementById('deposit');
+    
 
-      const previousTotalDepositString = initialDeposit.innerText
-      const previousTotalDeposit = parseFloat(previousTotalDepositString)
-      const newDeposited = parseFloat(newDepositedString)
-      
-      const currentTotalDeposit = previousTotalDeposit + newDeposited
+    else{
+        const initialDeposit =  document.getElementById('deposit');
 
-      initialDeposit.innerText = currentTotalDeposit
+        const previousTotalDepositString = initialDeposit.innerText
+        const previousTotalDeposit = parseFloat(previousTotalDepositString)
+        
+        
+        const currentTotalDeposit = previousTotalDeposit + newDeposited
+  
+        initialDeposit.innerText = currentTotalDeposit
+  
+        depositField.value = ''
+  
+        const initialBalanceString = document.getElementById('balance')
+        const initialBalanceValue = initialBalanceString.innerText
+        const initialBalance = parseFloat(initialBalanceValue)
+  
+        const totalBalance = initialBalance + newDeposited
+  
+        initialBalanceString.innerText = totalBalance
+    }
 
-      depositField.value = ''
-
-      const initialBalanceString = document.getElementById('balance')
-      const initialBalanceValue = initialBalanceString.innerText
-      const initialBalance = parseFloat(initialBalanceValue)
-
-      const totalBalance = initialBalance + newDeposited
-
-      initialBalanceString.innerText = totalBalance
+  
 
 
       
--- js/withdraw.js
+++ js/withdraw.js
@@ -2,22 +2,43 @@ document.getElementById('btn-withdraw').addEventListener('click' , function(){
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawString = withdrawField.value
     const newWithdraw = parseFloat(newWithdrawString)
+    
+    if(isNaN(newWithdraw)){
+        alert("Please enter numbers")
+    }
+    else{
+        const WithdrawAmount = document.getElementById('withdraw');
+        const previousWithdrawAmountString = WithdrawAmount.innerText;
+        const previousWithdrawAmount = parseFloat(previousWithdrawAmountString);
+    
+       
+    
+        withdrawField.value = ''
+    
+        const balance = document.getElementById('balance') ;
+        const balanceString  = balance.innerText
+        const previousBalance = parseFloat(balanceString) ;
+    
+        if(previousBalance < newWithdraw){
+            alert("You do not have sufficient balance you have " + " " +previousBalance + " Taka")
+        }
+    
+        else{
+            const totalBalance = previousBalance  - newWithdraw ;
+    
+            balance.innerText = totalBalance;
+            WithdrawAmount.innerText = previousWithdrawAmount + newWithdraw;
+        }
+    }
 
-    const WithdrawAmount = document.getElementById('withdraw');
-    const previousWithdrawAmountString = WithdrawAmount.innerText;
-    const previousWithdrawAmount = parseFloat(previousWithdrawAmountString);
+    
+       
 
-    WithdrawAmount.innerText = previousWithdrawAmount + newWithdraw;
 
-    withdrawField.value = ''
-
-    const balance = document.getElementById('balance') ;
-    const balanceString  = balance.innerText
-    const previousBalance = parseFloat(balanceString) ;
-
-    const totalBalance = previousBalance  - newWithdraw ;
-
-    balance.innerText = totalBalance;
+   
+    
+  
+   
 
 
 
